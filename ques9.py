ac6a36b67ee0c85a1aec7788a16c6b269c17d839
prabhjot315 <81428068+prabhjot315@users.noreply.github.com>
2021-04-12 18:41:58 +0530

    Add files via upload

---
+++ ques9.py
@@ -0,0 +1,24 @@
+def smallest_largest_words(str1):
+    word = "";
+    all_words = [];
+    str1 = str1 + " ";
+    for i in range(0, len(str1)):
+        if(str1[i] != ' '):
+            word = word + str1[i];  
+        else:
+            all_words.append(word);  
+            word = "";  
+          
+    small = large = all_words[0];    
+    for k in range(0, len(all_words)):
+        if(len(small) > len(all_words[k])):
+            small = all_words[k];
+        if(len(large) < len(all_words[k])):
+            large = all_words[k];
+    return small,large;
+
+str1 = input()
+print("Original Strings:\n",str1)
+small, large = smallest_largest_words(str1)  
+print("Smallest word: " + small);  
+print("Largest word: " + large);
